1162f4901409420499f97765e9669e45b627ac8f
23495A0543 <namesrinu18@gmail.com>
2024-11-17 10:02:19 +0530

    project day3

--- app.py
+++ app.py
@@ -7,4 +7,32 @@ def homepage():
     return render_template("index.html")
 
 
+@app.route("/addmission",methods =["GET"])
+def addmission():
+    return render_template("addmission.html")
+
+
+@app.route("/register",methods =["GET"])
+def register():
+    return render_template("register.html")
+
+
+
+
+@app.route("/view",methods =["GET"])
+def view():
+    return render_template("view.html")
+
+@app.route("/update",methods =["GET"])
+def update():
+    return render_template("update.html")
+
+@app.route("/delete",methods =["GET"])
+def delete():
+    return render_template("delete.html")
+
+@app.route("/campus_tour",methods =["GET"])
+def campus_tour():
+    return render_template("campus_tour.html")
+
 app.run(debug=True)
