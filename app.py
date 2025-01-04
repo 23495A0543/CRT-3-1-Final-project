777a0417e872a2842c5d6820067f3cd0fa362263
23495A0543 <namesrinu18@gmail.com>
2025-01-04 14:14:57 +0530

    final project

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request,redirect # type: ignore
+from pymongo import MongoClient 
 app = Flask(__name__)
 
 @app.route("/",methods =["GET"])
@@ -10,7 +10,17 @@ def homepage():
 @app.route("/addmission",methods =["GET"])
 def addmission():
     return render_template("addmission.html")
-
+@app.route("/callme",methods=["POST","GET"])
+def callme_details():
+    name= request.form["name"] 
+    Email= request.form["Email"]
+    number= request.form["number"]    
+    course =  request.form["course"]  
+    callme_details.insert_one({
+    
+        "name":name,"Email":Email,"number":number,"course":course
+    })
+    return redirect("/")
 
 @app.route("/register",methods =["GET"])
 def register():
